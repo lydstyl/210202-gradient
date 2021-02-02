69cc7c6e137bb828173f48ab2167c1ca6f83fb25
Gabriel Brun <lydstyl@gmail.com>
2021-02-02 14:28:49 +0100

    csv works

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,7 +1,34 @@
-import { hello } from '../app/index'
+import { avg, gradient, sort123, gradientForAvg } from '../app/index'
 
 describe('Main index test suite', () => {
-  it('show hello world', () => {
-    expect(hello('world')).toBe('Hello world')
+  it('calculate orders average', () => {
+    expect(
+      avg([
+        { quantity: 2, price: 10 },
+        { quantity: 1, price: 20 },
+      ]),
+    ).toBe(13.333333333333334)
+  })
+
+  it('give gradient orders', () => {
+    expect(
+      gradient([
+        1,2,3
+      ], 0.1),
+    ).toEqual(
+      [
+        {quantity:100, price:1},
+        {quantity:90, price:2},
+        {quantity:80, price:3}
+      ]
+    )
+  })
+
+  it('sort prices like so 1, 2, 3', ()=>{
+    expect(sort123([10,11,8,7,12])).toEqual([7,8,10,11,12])
+  })
+
+  it('give a gradient that avg is less then asked', () => {
+    expect(avg(gradientForAvg([1,2,3], 1.9))).toBeLessThanOrEqual(1.9)
   })
 })
